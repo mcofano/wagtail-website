604438f549688aa3c172af734f1d28ad4a2adaf3
marcocofano <marco.cofano@edreamsodigeo.com>
2020-09-23 22:08:46 +0200

    create basic models. add homepage and about (flex) page

---
+++ train_climb/train_climb/settings/dev.py
@@ -0,0 +1,28 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'd**-4=dxmsvyrkj97ejt=x=3zcbq#^57-&omzb(5g%)@fw0wn@'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+INSTALLED_APPS = INSTALLED_APPS + [
+    'debug_toolbar'
+]
+
+MIDDLEWARE = MIDDLEWARE + [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = ('127.0.0.1', '172.17.0.1')
+STATIC_URL = '/static/'
+
+try:
+    from .local import *
+except ImportError:
+    pass
