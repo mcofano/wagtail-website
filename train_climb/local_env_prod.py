1b71079030b58bd5cfe2819a04620887411333b7
marcocofano <marco.cofano@edreamsodigeo.com>
2021-06-14 17:16:24 +0200

    mysql add in base.py.

---
+++ train_climb/local_env_prod.py
@@ -0,0 +1,2 @@
+''' rename this file to local_env.txt and fill the requested fields'''
+MYSQL_PASSWORD = ''
--- train_climb/train_climb/settings/base.py
+++ train_climb/train_climb/settings/base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from local_env import MYSQL_PASSWORD
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
@@ -103,9 +104,17 @@ WSGI_APPLICATION = 'train_climb.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'default1': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'default': {
+        'NAME': 'train_climb',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'marcocofano',
+        'PASSWORD': MYSQL_PASSWORD,
+        'HOST': 'localhost',
+        'port': 3306
     }
 }
 
